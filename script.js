370bacd569806453095ad8192f780692636262bb
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-11 17:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,53 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
- function creatPromise() {
-	 return new Promise((resolve)=>{
-		 
-	 })
- 	
- }
+const loading = document.createElement("div");
+loading.id = "loading";
+loading.innerText = "Loading...";
+output.appendChild(loading);
+
+const error = document.createElement("div");
+error.id = "error";
+output.appendChild(error);
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => {
+      resolve(img);
+    };
+    img.onerror = () => {
+      reject(`Failed to download image: ${url}`);
+    };
+    img.src = url;
+  });
+}
+function downloadImages() {
+
+  loading.style.display = "block";
+  error.innerText = "";
+
+  const promises = images.map(image => {
+    return downloadImage(image.url);
+  });
+
+  Promise.all(promises)
+    .then(downloadedImages => {
+
+      loading.style.display = "none";
+
+      downloadedImages.forEach(img => {
+        output.appendChild(img);
+      });
+
+    })
+    .catch(err => {
+
+      loading.style.display = "none";
+      error.innerText = err;
+
+    });
+}
+
+
+downloadImages();
